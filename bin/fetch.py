e637da195bd6837f14c2e20ce0804fec6fcb6346
Paul Fenwick <pjf@perltraining.com.au>
2017-02-27 18:06:42 +1300

    [TAAS-1] Added end-to-end downloading to JSON generation.
    Also discovered our spreadsheet has a bad line with no ID.

--- bin/fetch.py
+++ bin/fetch.py
@@ -5,13 +5,19 @@
 # changes.
 
 import taas
+import json
 
 def main():
     config = taas.read_config()
     for name in config['sources']:
         sheet = config['sources'][name]
 
-        taas.save_google_sheet(name,sheet['key'],sheet['gid'])
+        taas.google_sheet_to_json(name, sheet['key'], sheet['gid'], sheet['mapping'])
+
+        # taas.save_google_sheet(name,sheet['key'],sheet['gid'])
+        # raw = taas.read_sheet_csv(name)
+        # cooked = taas.normalise_sheet(raw, sheet['mapping'])
+        # taas.save_json(name, cooked)
 
 if __name__ == "__main__":
     main()
--- taas.py
+++ taas.py
@@ -1,3 +1,5 @@
+import csv
+import json
 import os
 import sys
 import urllib
@@ -35,6 +37,25 @@ def read_config(file = None):
     with open(file) as stream:
         return yaml.load(stream)
 
+def read_sheet_csv(name, directory = None):
+    """
+        Reads the sheet specified, and returns a object (array-of-dicts)
+        that contains its data. The first row of the sheet will be used
+        to name the dictionary keys.
+
+        >>> sheet = read_sheet_csv(some_sheet)
+        >>> for row in sheet:
+        ...     print(row['some field'], row['another field'])
+    """
+
+    if directory is None:
+        directory = sheets_root();
+
+    path = os.path.join(directory, name + ".csv")
+    debug("Reading CSV values from {}\n".format(path))
+
+    return csv.DictReader(open(path))
+
 def save_google_sheet(name,key,gid,file_format="csv",directory=None):
     """
         Downloads a google sheet to "name.file_format" (eg: "function_roles.csv").
@@ -59,4 +80,55 @@ def save_google_sheet(name,key,gid,file_format="csv",directory=None):
 
     # TODO: How does urllib handle errors? Can we trust it to throw on failure?
     urllib.urlretrieve(url, filename)
+
+def normalise_sheet(raw, mapping):
+    """
+        Converts data from the form found in .csv files to that which will be served by
+        our final JSON system.
+
+        Raw is the data as produced from read_sheet_csv().
+        Mapping is a dict of cooked->raw pairs (eg: 'label' : 'Preferred Term')
+    """
+
+    cooked = []
+
+    for row in raw:
+        item = {}
+        for field in mapping:
+            item[field] = row[mapping[field]]
+        cooked.append(item)
+
+    return cooked
+
+def save_json(name, data, directory = None):
+    """
+        Saves `data` as JSON into "directory/name.json".
+        If no directory is specified, sheet_roots() will be used.
+    """
+
+    if directory is None:
+        directory = sheets_root()
+
+    path = os.path.join(directory, "{}.json".format(name))
+
+    debug("Writing to {}\n".format(path))
+
+    with open(path,"w") as jsonfile:
+        json.dump(data, jsonfile, indent=4, sort_keys=True)
+
+def google_sheet_to_json(name, key, gid, mapping, directory = None):
+    """
+        Does the entire process of downloading a google sheet, mapping
+        the fields, and saving it as JSON.
+
+        The directory defaults to `sheets_root()` if none provided.
+    """
+
+    if directory is None:
+        directory = sheets_root()
+
+    save_google_sheet(name,key,gid)
+    raw = read_sheet_csv(name)
+    cooked = normalise_sheet(raw, mapping)
+    save_json(name, cooked)
 
