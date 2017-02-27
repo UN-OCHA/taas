aecf0011e22c815745910460a07e9d263c1d9d3c
Paul Fenwick <pjf@perltraining.com.au>
2017-02-27 14:18:36 +1300

    [TAAS-1] Split config into separate config.yml
    This opens up the path for a much nicer pipeline, as well as config CI
    testing.

--- bin/fetch.py
+++ bin/fetch.py
@@ -5,18 +5,13 @@
 import os
 import sys
 import urllib
+import yaml
 
-# TODO: It would be great to have these in a config file.
-sheets = [
-    {
-        'name'   : 'functional-roles',
-        'key'    : '1c9wehuauQAAegElIRI6vhWktKSI-PcPjHHiXdqASonk',
-        'gid'    : 0,
-    }
-]
+# We use os.path.dirname rather than examining __file__ as it survives py2exe and other tricks.
+bin_path    = os.path.dirname(sys.argv[0])
+config_path = os.path.join(bin_path,"..","config.yml")
 
 # Where to store our files.
-# We use os.path.dirname rather than examining __file__ as it survives py2exe and other tricks.
 dst_path = os.path.join(
     os.path.dirname(sys.argv[0]),
     "..",
@@ -27,15 +22,21 @@ dst_path = os.path.join(
 export_base_url = "https://docs.google.com/feeds/download/spreadsheets/Export?exportFormat=csv"
 
 def main():
-    for sheet in sheets:
+    config = read_config(config_path)
+    for name in config['sources']:
+        sheet = config['sources'][name]
 
-        save_as = os.path.join(dst_path, sheet['name'] + ".csv")
+        save_as = os.path.join(dst_path, name + ".csv")
 
         url = "{}&key={}&gid={}".format(export_base_url, sheet['key'], sheet['gid'])
 
         # TODO: How does urllib handle errors? Can we trust it to throw on failure?
         debug("Saving {} to {}\n".format(url, save_as))
         urllib.urlretrieve(url, save_as)
+
+def read_config(file):
+    with open(file) as stream:
+        return yaml.load(stream)
 
 def debug(string):
     print string
