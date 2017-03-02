c747f6b3ae5629f067528901c1676b898bad3e4b
Paul Fenwick <pjf@perltraining.com.au>
2017-03-02 21:35:50 +1300

    fetch.py: Remove old code that's integrated into taas library.

--- bin/fetch.py
+++ bin/fetch.py
@@ -14,10 +14,5 @@ def main():
 
         taas.google_sheet_to_json(name, sheet['key'], sheet['gid'], sheet['mapping'])
 
-        # taas.save_google_sheet(name,sheet['key'],sheet['gid'])
-        # raw = taas.read_sheet_csv(name)
-        # cooked = taas.normalise_sheet(raw, sheet['mapping'])
-        # taas.save_json(name, cooked)
-
 if __name__ == "__main__":
     main()
